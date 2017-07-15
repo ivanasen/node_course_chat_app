9a969e2dd47926cdbf13f297f5b989bf085e4092
vankata9907 <vankata9907@gmail.com>
2017-07-15 13:20:06 +0300

    Fixed static folder bug

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 require('./config/config');
 const express = require('express');
+const path = require('path');
 
+const publicPath = path.join(__dirname, '../public');
 const app = express();
-app.use(express.static('public'));
+app.use(express.static(publicPath));
 
 app.listen(process.env.PORT, () => {
     console.log(`App started on port ${process.env.PORT}`);
