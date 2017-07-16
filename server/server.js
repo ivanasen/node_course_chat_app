fe7bfc9c82f75c2701fe805cea1c5fb3a03e25a8
vankata9907 <vankata9907@gmail.com>
2017-07-16 12:47:21 +0300

    Added Messaging IO

--- server/server.js
+++ server/server.js
@@ -1,11 +1,35 @@
 require('./config/config');
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.emit('newMessage', {
+        from: 'Pesho123',
+        text: 'EEeeeeee gosho be',
+        createdAt: 123
+    });
+
+    socket.on('createMessage', (message) => {
+        message.createdAt = new Date();
+        socket.emit('newMessage', message);
+    });
+    
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+});
+
+server.listen(process.env.PORT, () => {
     console.log(`App started on port ${process.env.PORT}`);
 });
